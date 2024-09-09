b6c5a8abd3b6191e1bd6575170104b1706e55b35
Simon <bosen.xu@mail.utoronto.ca>
2024-09-09 12:10:52 -0400

    Edited Multiples file with comments.

--- src/Multiples.java
+++ src/Multiples.java
@@ -8,7 +8,7 @@ public class Multiples {
 
     private static int doMultiplesLoop(int k) {
         int count = 0;
-        for (int i = 1; i < k; i++) {
+        for (int i = 1; i < k; i++) { // Start at 1 to avoid counting 0
             if (i % 3 == 0 || i % 5 == 0){
                 count ++;
             }
@@ -17,9 +17,11 @@ public class Multiples {
     }
 
     private static int doMultiplesMath(int k) {
-        int multiplesOf3 = k / 3;
-        int multiplesOf5 = k / 5;
-        int multiplesOfBoth = k / 15;
+        // Only works when the two numbers are not factors of each other
+        int j = k - 1; // We are not including 1000
+        int multiplesOf3 = j / 3;
+        int multiplesOf5 = j / 5;
+        int multiplesOfBoth = j / 15;
         return multiplesOf3 + multiplesOf5 - multiplesOfBoth;
     }
 }
